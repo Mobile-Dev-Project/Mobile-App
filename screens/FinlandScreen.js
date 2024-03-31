a6d5e1838d2daeb3998c6d84e613ad8afa00dc9d
Muna Mahadi <munammahadi@gmail.com>
2024-03-31 11:58:02 +0300

    Fix circular import

--- screens/FinlandScreen.js
+++ screens/FinlandScreen.js
@@ -11,7 +11,7 @@ import { Video } from "expo-av";
 import { Linking } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import HomeScreen from "./HomeScreen";
+const HomeScreen = React.lazy(() => import("./HomeScreen"));
 import "react-native-gesture-handler";
 
 // Define the destinations array outside of your FinlandScreen component
