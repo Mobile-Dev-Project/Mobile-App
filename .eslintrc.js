ff1143c08eac936caa51d535d578630f1a5ce3ac
Muna Mahadi <munammahadi@gmail.com>
2024-04-03 09:42:30 +0300

    Github actions fix-3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-    root: true,
-    extends: ['@react-native-community'],
-    // Your custom rules here
-  };
-  
+        root: true,
+        extends: ['@react-native-community'],
+        // Your custom rules here
+        rules: {
+        'prettier/prettier': 'error',
+        },
+        plugins: ['prettier'],
+        extends: [
+        'plugin:prettier/recommended',
+        ],
+    };  
